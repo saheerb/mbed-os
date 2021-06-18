7521f5f31b779c5a2d6ca8f24cb37c629bf172de
saheerb <saheer.babu@arm.com>
2021-06-18 12:22:33 +0100

    join several workflows together

--- docker_images/mbed-os-env/test.sh
+++ docker_images/mbed-os-env/test.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 set -x
 MBED_OS_VERSION=$1
-if master; then
-EXAMPL_VERSION="development"
-else
-# if tagged if tag is available us it
-# if tag is not available? use development
-fi
+# if master; then
+# EXAMPL_VERSION="development"
+# else
+# # if tagged if tag is available us it
+# # if tag is not available? use development
+# fi
 
 mbed import mbed-os-example-blinky
 cd mbed-os-example-blinky
