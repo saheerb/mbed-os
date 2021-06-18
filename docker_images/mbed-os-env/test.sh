602d9578b60a4d565a695414790677731bd821cf
saheerb <saheer.babu@arm.com>
2021-06-18 12:11:43 +0100

    join several workflows together

--- docker_images/mbed-os-env/test.sh
+++ docker_images/mbed-os-env/test.sh
@@ -1,9 +1,17 @@
 #!/bin/sh
 set -x
 MBED_OS_VERSION=$1
+if master; then
+EXAMPL_VERSION="development"
+else
+# if tagged if tag is available us it
+# if tag is not available? use development
+fi
+
 mbed import mbed-os-example-blinky
 cd mbed-os-example-blinky
 git checkout ${MBED_OS_VERSION}
 mbed deploy 
+# symlink
 mbed compile -m K64F -t GCC_ARM
 mbed-tools compile -m K64F -t GCC_ARM
