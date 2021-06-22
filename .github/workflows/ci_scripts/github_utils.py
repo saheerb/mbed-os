d6f8747197e32ea0be0038edf721b928f9b20b82
saheerb <saheer.babu@arm.com>
2021-06-22 10:25:41 +0100

    old images delete

--- .github/workflows/ci_scripts/github_utils.py
+++ .github/workflows/ci_scripts/github_utils.py
@@ -1,42 +1,44 @@
-import time
-import re
-import os
 import click
 import requests
-import json
 import logging
 import sys
-import subprocess
+import time
 
-
+# delete images older than provided argument (number_of_days)
 @click.command()
 @click.pass_context
 @click.option("-r", "--repository", required=True)
-@click.option("-t", "--tag", required=True)
-def delete_image(ctx, repository, tag):
+@click.option(
+    "-n",
+    "--number_of_days",
+    default="1",
+    help="number of days since image was created",
+    required=False,
+)
+def delete_old_images(ctx, repository, number_of_days):
     s = requests.Session()
-    github_api_accept = 'application/vnd.github.v3+json'
-    s.headers.update({'Authorization': f'token {ctx.obj["passwd"]}', 'Accept': github_api_accept})
-    r = s.get(f'https://api.github.com/user/packages/container/{repository}/versions')
+    github_api_accept = "application/vnd.github.v3+json"
+    s.headers.update(
+        {"Authorization": f'token {ctx.obj["passwd"]}', "Accept": github_api_accept}
+    )
+    r = s.get(f"https://api.github.com/user/packages/container/{repository}/versions")
     versions = r.json()
+
     version_id = None
+    pattern = "%d.%m.%Y %H:%M:%S"
+    pattern = "%Y-%m-%dT%H:%M:%SZ"
+    current_time = time.time()
+
     for version in versions:
-        try:
-            if tag in version['metadata']['container']['tags']:
-                version_id = (version['id'])
-                break
-        except:
-            pass
+        epoch = int(time.mktime(time.strptime(version["updated_at"], pattern)))
 
-    url = f'https://api.github.com/user/packages/container/{repository}/versions/{version_id}'
-    resp = s.delete(url)
-    resp.raise_for_status()
+        if (current_time - epoch) / (24 * 60 * 60) > int(number_of_days):
+            version_id = version["id"]
+            logging.debug(f"deleteing image with version id {version_id}")
 
-    if resp.status_code != 204:
-        logging.error(resp.status_code)
-        logging.error(resp)
-        logging.error("Request failed, check credentials")
-        sys.exit(1)
+            url = f"https://api.github.com/user/packages/container/{repository}/versions/{version_id}"
+            resp = s.delete(url)
+            resp.raise_for_status()
 
 
 @click.group()
@@ -45,10 +47,7 @@ def delete_image(ctx, repository, tag):
 @click.option("-p", "--passwd", required=False)
 @click.option("-v", "--verbose", is_flag=True, default=False)
 def main(ctx, username, passwd, verbose):
-    ctx.obj = {
-        "username": username,
-        "passwd": passwd
-    }
+    ctx.obj = {"username": username, "passwd": passwd}
 
     if verbose:
         logging.basicConfig(
@@ -66,5 +65,5 @@ def main(ctx, username, passwd, verbose):
 
 
 if __name__ == "__main__":
-    main.add_command(delete_image)
+    main.add_command(delete_old_images)
     main()
