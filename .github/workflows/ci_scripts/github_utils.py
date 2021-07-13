86160a1d00b9d4eb9a25a9a6165260cd6ad73e95
Saheer <saheerb@gmail.com>
2021-07-13 18:35:20 +0100

    reset

--- .github/workflows/ci_scripts/github_utils.py
+++ .github/workflows/ci_scripts/github_utils.py
@@ -3,6 +3,37 @@ import requests
 import logging
 import sys
 import time
+import json
+import subprocess
+
+@click.command()
+@click.pass_context
+@click.option("-r", "--repository", required=True)
+@click.option("-t", "--tag", required=True)
+@click.option("-p", "--platform", required=False)
+def get_digest(ctx, repository, tag, platform=None):
+    command =  f"docker run quay.io/skopeo/stable --creds={ctx.obj['username']}:{ctx.obj['passwd']} inspect docker://ghcr.io/{ctx.obj['username']}/{repository}:{tag} --raw"
+    output = subprocess.run(command.split(), stdout=subprocess.PIPE).stdout.decode('utf-8')
+    output = json.loads(output)
+    images = output["manifests"]
+    digest = ""
+    if len(images) > 1 and platform == None:
+        logging.error(
+            "This tag has more than one platform associated to it, please input a platform"
+        )
+        sys.exit(1)
+
+
+    for image in images:
+        if platform != None:
+            if (platform.split("/")[0] == image["platform"]["os"]) and (
+                platform.split("/")[1] == image["platform"]["architecture"]
+            ):
+                digest = image["digest"]
+        else:
+            digest = image["digest"]
+
+    print(digest)
 
 # delete images older than provided argument (number_of_days)
 @click.command()
@@ -65,5 +96,6 @@ def main(ctx, username, passwd, verbose):
 
 
 if __name__ == "__main__":
+    main.add_command(get_digest)
     main.add_command(delete_old_images)
     main()
